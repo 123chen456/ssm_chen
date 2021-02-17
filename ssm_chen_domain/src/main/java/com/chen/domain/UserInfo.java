38f9a8df5181e3bedf539f221d968d1c9affbea5
陈建局 <m15138509205@163.com>
2021-02-17 11:07:15 +0800

    增加 string3

--- ssm_chen_domain/src/main/java/com/chen/domain/UserInfo.java
+++ ssm_chen_domain/src/main/java/com/chen/domain/UserInfo.java
@@ -7,6 +7,7 @@ public class UserInfo {
     private String id;
     private String string1;
     private String string2;
+    private String string3;
     private String username;
     private String email;
     private String password;
@@ -14,6 +15,22 @@ public class UserInfo {
     private int status;
     private String statusStr;
     private List<Role> roles;
+
+    public String getString1() {
+        return string1;
+    }
+
+    public void setString1(String string1) {
+        this.string1 = string1;
+    }
+
+    public String getString3() {
+        return string3;
+    }
+
+    public void setString3(String string3) {
+        this.string3 = string3;
+    }
 
     public String getString2() {
         return string2;
